89076032130232126a0afa84c7f9098b86092a57
Bruno Rijsman <brunorijsman@gmail.com>
2018-07-04 09:07:42 -0700

    Encode and decode a LIE message.

--- thrift_learn.py
+++ thrift_learn.py
@@ -18,3 +18,26 @@ def decode(encoded_msg):
     lie_packet = LIEPacket()
     lie_packet.read(protocol_in)
     return lie_packet
+
+lie_packet_before = LIEPacket(
+    name = '',
+    local_id = None,
+    flood_port = 912,        # TODO: Is this the right default?
+    link_mtu_size = 1400,
+    neighbor = None,
+    pod = 0,
+    nonce = None,
+    capabilities = None,
+    holdtime = 3,            # TODO: Should this be hold_time?
+    not_a_ztp_offer = False,
+    you_are_not_flood_repeater = False,
+    label = None)
+
+print("Before encoding: ", lie_packet_before, "\n")
+
+encoded_msg = encode(lie_packet_before)
+print("Encoded message: ", encoded_msg, "\n")
+
+lie_packet_after = decode(encoded_msg)
+print("Adter decoding: ", lie_packet_after, "\n")
+
