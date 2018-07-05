64e27afd5b9f505e578f0edb73a354550cd08ff2
Bruno Rijsman <brunorijsman@gmail.com>
2018-07-05 08:31:49 -0700

    Add placeholder for MTU

--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 import os
 import socket
+from fcntl import ioctl
 from multicast_send_handler import MulticastSendHandler
 from multicast_receive_handler import MulticastReceiveHandler
 from timer import Timer
@@ -10,6 +11,7 @@ from encoding.ttypes import PacketHeader, PacketContent, LIEPacket, ProtocolPack
 # TODO: send and receive LIE message on a per interface basis
 # TODO: Bind the socket to the interface (send and receive packets on that specific interface)
 # TODO: LIEs arriving with a TTL larger than 1 MUST be ignored.
+# TODO: Find a way to detect MTU changes
 
 class Interface:
 
@@ -21,11 +23,19 @@ class Interface:
             hostname = format(system_id, 'x')
         return hostname + '-' + format(pid) + '-' + short_name
 
+    @staticmethod
+    def get_mtu(interface_name):
+        # TODO: Find a portable (or even non-portable) way to get the interface MTU
+        # TODO: Find a way to be informed whenever the interface MTU changes
+        mtu = 1500
+        return mtu
+
     def __init__(self, short_name, node):
         self._node = node
         self._short_name = short_name
         self._long_name = Interface.generate_long_name(short_name, node.system_id)
         self._local_id = node.allocate_interface_id()
+        self._mtu = Interface.get_mtu(short_name)
         self._multicast_send_handler = MulticastSendHandler(
             node.lie_ipv4_multicast_address, 
             node.lie_destination_port)
@@ -44,7 +54,7 @@ class Interface:
             name = self._long_name,
             local_id = self._local_id,
             flood_port = self._node.tie_destination_port,
-            link_mtu_size = 1400,
+            link_mtu_size = self._mtu,
             neighbor = None,
             pod = 0,
             nonce = None,
