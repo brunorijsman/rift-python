35484f79676402fd866789a37f4573f493bec536
Bruno Rijsman <brunorijsman@gmail.com>
2018-07-04 20:18:34 -0700

    Minor refactor + advertise name in TIE

--- interface.py
+++ interface.py
@@ -1,8 +1,10 @@
 import os
+import socket
 from multicast_send_handler import MulticastSendHandler
 from multicast_receive_handler import MulticastReceiveHandler
 from timer import Timer
-from packet_encoding import *
+from packet_common import create_packet_header, encode_protocol_packet, decode_protocol_packet
+from encoding.ttypes import PacketHeader, PacketContent, LIEPacket, ProtocolPacket
 
 # TODO: make it possible to enable or disable RIFT on a per interface basis
 # TODO: send and receive LIE message on a per interface basis
@@ -11,28 +13,57 @@ from packet_encoding import *
 
 class Interface:
 
-    def __init__(self, name, config):
-        self._name = name
-        self._config = config
+    @staticmethod
+    def generate_long_name(short_name, system_id):
+        hostname = socket.gethostname()
+        pid = os.getpid() 
+        if not hostname:
+            hostname = format(system_id, 'x')
+        return hostname + '-' + format(pid) + '-' + short_name
+
+    def __init__(self, short_name, node):
+        self._short_name = short_name
+        self._long_name = Interface.generate_long_name(short_name, node.system_id)
+        self._node = node
         self._multicast_send_handler = MulticastSendHandler(
-            config.lie_ipv4_multicast_address, 
-            config.lie_destination_port)
+            node.lie_ipv4_multicast_address, 
+            node.lie_destination_port)
         self._multicast_receive_handler = MulticastReceiveHandler(
-            config.lie_ipv4_multicast_address, 
-            config.lie_destination_port,
+            node.lie_ipv4_multicast_address, 
+            node.lie_destination_port,
             self.receive_multicast_message)
         self._send_timer = Timer(
-            config.lie_send_interval_secs,
+            node.lie_send_interval_secs,
             self.send_lie_protocol_packet)
+
+    def create_lie_protocol_packet(self):
+        packet_header = create_packet_header(self._node)
+        # TODO use information in config to create LIE Packet
+        lie_packet = LIEPacket(
+            name = self._long_name,
+            local_id = None,
+            flood_port = 912,        # TODO: Is this the right default?
+            link_mtu_size = 1400,
+            neighbor = None,
+            pod = 0,
+            nonce = None,
+            capabilities = None,
+            holdtime = 3,            # TODO: Should this be hold_time?
+            not_a_ztp_offer = False,
+            you_are_not_flood_repeater = False,
+            label = None)
+        packet_content = PacketContent(lie = lie_packet)
+        protocol_packet = ProtocolPacket(packet_header, packet_content)
+        return protocol_packet
+
+    def send_lie_protocol_packet(self):
+        protocol_packet = self.create_lie_protocol_packet()
+        encoded_protocol_packet = encode_protocol_packet(protocol_packet)
+        self._multicast_send_handler.send_message(encoded_protocol_packet)
+        print("Sent LIE Protocol Packet {}".format(protocol_packet))
 
     def receive_multicast_message(self, message):
         protocol_packet = decode_protocol_packet(message)
         # TODO: Dispatch, depending on message type
         print("Received Protocol Packet {}".format(protocol_packet))
-
-    def send_lie_protocol_packet(self):
-        protocol_packet = create_lie_protocol_packet(self._config)
-        encoded_protocol_packet = encode_protocol_packet(protocol_packet)
-        self._multicast_send_handler.send_message(encoded_protocol_packet)
-        print("Sent LIE Protocol Packet {}".format(protocol_packet))
 
--- packet_common.py
+++ packet_common.py
@@ -6,35 +6,19 @@ sys.path.append('gen-py')
 
 from thrift.protocol import TBinaryProtocol
 from thrift.transport import TTransport
-from encoding.ttypes import PacketHeader, PacketContent, LIEPacket, ProtocolPacket
+from encoding.ttypes import PacketHeader, ProtocolPacket
 
 RIFT_MAJOR_VERSION = 11
 RIFT_MINOR_VERSION = 0
 
-def create_lie_protocol_packet(node):
+def create_packet_header(node):
     packet_header = PacketHeader(
         major_version = RIFT_MAJOR_VERSION,
         minor_version = RIFT_MINOR_VERSION,
         sender = node.system_id,
         level = node.advertised_level
     )
-    # TODO use information in config to create LIE Packet
-    lie_packet = LIEPacket(
-        name = '',
-        local_id = None,
-        flood_port = 912,        # TODO: Is this the right default?
-        link_mtu_size = 1400,
-        neighbor = None,
-        pod = 0,
-        nonce = None,
-        capabilities = None,
-        holdtime = 3,            # TODO: Should this be hold_time?
-        not_a_ztp_offer = False,
-        you_are_not_flood_repeater = False,
-        label = None)
-    packet_content = PacketContent(lie = lie_packet)
-    protocol_packet = ProtocolPacket(packet_header, packet_content)
-    return protocol_packet
+    return packet_header
 
 def encode_protocol_packet(protocol_packet):
     transport_out = TTransport.TMemoryBuffer()
