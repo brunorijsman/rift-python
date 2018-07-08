7c1b8cf4eb0d0c5b4d588206ae11b0fbfd69ad19
Bruno Rijsman <brunorijsman@gmail.com>
2018-07-08 15:48:45 -0700

    Make print_adjacency_fsm work again

--- print_adjacency_fsm.py
+++ print_adjacency_fsm.py
@@ -1,18 +1,19 @@
 from argparse import ArgumentParser
-from fsm import FiniteStateMachine
+from node import Node
 from interface import Interface
+
+# TODO: report flags
+#       i = implicit = there is no transition for event E in state S (implicitly do nothing)
+#       d = do-nothing = there is a do-nothing transition for event E in state S back to state S
+#       s = state-independent = the event E is handled exactly the same in every state S and goes back to S
+# TODO: report state-entry actions
 
 parser = ArgumentParser(description='Print adjacency Finite State Machine (FSM)')
 parser.add_argument('--report-missing', action='store_true', help='Report missing transitions')
 args = parser.parse_args()
 
-fsm = FiniteStateMachine(
-        state_enum = Interface.State, 
-        event_enum = Interface.Event, 
-        transitions = Interface.transitions, 
-        initial_state = Interface.State.ONE_WAY,
-        action_handler = None,
-        log = None,
-        log_id = None)
-fsm.print_transition_table(args.report_missing)
+node = Node()
+interface = Interface("dummy", node)
+
+interface._fsm.print_transition_table(args.report_missing)
 exit(0)
