1d554f02fb00b980485b87258ed989bca7be276b
ilyav11 <vershkov.ml@gmail.com>
2018-07-15 00:22:32 +0300

    Adding specificaiton description and some renaming

--- ztp.py
+++ ztp.py
@@ -26,8 +26,8 @@ class Ztp:
     # on  CHANGE_LOCAL_LEAF_INDICATIONS in UpdatingClients    finishes in    ComputeBestOffer: STORE_LEAF_FLAGS
     # on  LOST_HAT in UpdatingClients    finishes in ComputeBestOffer: NO_ACTION
     # on    BETTER_HAT in HOLDING_DOWN    finishes in HoldingDown: NO_ACTION
-    # on    NeighborOffer in ComputeBestOffer    finishes in ComputeBestOffer: NO_ACTION
-    #     if no level offered
+    # on    NeighborOffer in ComputeBestOffer    finishes in ComputeBestOffer: action_update_or_remove_offer
+    #     if NeighborOffer.no_level_offered
     #           then REMOVE_OFFER
     #           else  if level > leaf
     #                   then UPDATE_OFFER
@@ -36,36 +36,36 @@ class Ztp:
     # on    ChangeLocalConfiguredLevel in HOLDING_DOWN    finishes in    ComputeBestOffer: STORE_LEVEL
     # on    BetterHAL in ComputeBestOffer    finishes in ComputeBestOffer:    LEVEL_COMPUTE
     # on    HoldDownExpired in HOLDING_DOWN    finishes in ComputeBestOffer:    PURGE_OFFERS
-    # on    ShortTic in HOLDING_DOWN    finishes in HOLDING_DOWN:
+    # on    ShortTic in HOLDING_DOWN    finishes in HOLDING_DOWN: action_check_hold_time_expired
     #    if HOLDDOWN_TIMER_EXPIRED
     #           then PUSH_EVENT   HOLDDOWN_EXPIRED
     # on    ComputationDone in ComputeBestOffer    finishes in    UpdatingClients: NO_ACTION
-    # on    LostHAL in UpdatingClients    finishes in HOLDING_DOWN:
+    # on    LostHAL in UpdatingClients    finishes in HOLDING_DOWN: action_update_holddown_timer_on_lost_hal
     #     if any southbound adjacencies present
     #         then update holddown timer to normal duration
     #         else fire   holddown    timer    immediately
-    # on     NeighborOffer in UpdatingClients    finishes in UpdatingClients:
+    # on     NeighborOffer in UpdatingClients    finishes in UpdatingClients: action_update_or_remove_offer
     #     if no level offered REMOVE_OFFER else
     #     if level > leaf then UPDATE_OFFER else REMOVE_OFFER
-    # on ChangeLocalConfiguredLevel in ComputeBestOffer finishes in ComputeBestOffer: store CONFIGURED level and LEVEL_COMPUTE
-    # on NeighborOffer in HoldingDown finishes in HoldingDown:
+    # on ChangeLocalConfiguredLevel in ComputeBestOffer finishes in ComputeBestOffer:  action_store_level_and_compute_level
+    # on NeighborOffer in HoldingDown finishes in HoldingDown: action_update_or_remove_offer
     #     if no level offered REMOVE_OFFER else
     #     if level > leaf then UPDATE_OFFER else REMOVE_OFFER
-    # on LostHAL in ComputeBestOffer finishes in HoldingDown:
+    # on LostHAL in ComputeBestOffer finishes in HoldingDown: action_update_holddown_timer_on_lost_hal
     #     if any    southbound    adjacencies    present
     #         then     update    holddown    timer    to    normal duration
     #         else fire holddown timer immediately
     # on BetterHAT in ComputeBestOffer finishes in ComputeBestOffer: LEVEL_COMPUTE
     # on WithdrawNeighborOffer in ComputeBestOffer finishes in ComputeBestOffer: REMOVE_OFFER
-    # on ChangeLocalLeafIndications in ComputeBestOffer finishes in       ComputeBestOffer: store leaf flags and LEVEL_COMPUTE
-    # on BetterHAL in HoldingDown finishes in HoldingDown: no action
+    # on ChangeLocalLeafIndications in ComputeBestOffer finishes in   ComputeBestOffer: action_store_leaf_flag_and_level_compute
+    # on BetterHAL in HoldingDown finishes in HoldingDown: NO_ACTION
     # on WithdrawNeighborOffer in HoldingDown finishes in HoldingDown:      REMOVE_OFFER
-    # on ChangeLocalLeafIndications in HoldingDown finishes in      ComputeBestOffer: store leaf flags
-    # on ChangeLocalConfiguredLevel in UpdatingClients finishes in      ComputeBestOffer: store level
-    # on ComputationDone in HoldingDown finishes in HoldingDown:
-    # on BetterHAL in UpdatingClients finishes in ComputeBestOffer: no      action
+    # on ChangeLocalLeafIndications in HoldingDown finishes in      ComputeBestOffer: action_store_leaf_flag
+    # on ChangeLocalConfiguredLevel in UpdatingClients finishes in      ComputeBestOffer: action_store_level
+    # on ComputationDone in HoldingDown finishes in HoldingDown: NO_ACTION
+    # on BetterHAL in UpdatingClients finishes in ComputeBestOffer: NO_ACTION
     # on WithdrawNeighborOffer in UpdatingClients finishes in      UpdatingClients: REMOVE_OFFER
-    # on Entry into UpdatingClients: update all LIE FSMs with      computation results
+    # on Entry into UpdatingClients: action_update_all_lie_fsm_with_computation_results
     # on Entry into ComputeBestOffer: LEVEL_COMPUTE
 
    #  Following words are used for well known procedures:
@@ -99,7 +99,7 @@ class Ztp:
         self.action_level_compute()
 
     #
-    def action_update_offer_if_non_null(self):
+    def action_update_or_remove_offer(self):
         # TODO:
         #          if no level offered REMOVE_OFFER else
         #          if level > leaf then UPDATE_OFFER else REMOVE_OFFER
@@ -114,7 +114,7 @@ class Ztp:
 
     def action_store_level_and_compute_level(self):
         #
-        #TODO what is first store level or compute level?
+        #store CONFIGURED level and LEVEL_COMPUTE
 
         self.action_store_level()
         self.action_level_compute()
@@ -141,8 +141,27 @@ class Ztp:
             self._fsm.push_event(self.Event.HOLD_DOWN_EXPIRED)
 
 
+    def action_update_all_lie_fsm_with_computation_results(self):
+        # TODO:
+        pass
+    #
 
+    def action_update_holddown_timer_on_lost_hal(self):
 
+        #     if any southbound adjacencies present
+        #         then update holddown timer to normal duration
+        #         else fire   holddown    timer    immediately
+
+        # TODO:
+        pass
+
+    #
+
+    def action_update_holddown_timer_on_lost_hal(self):
+        # TODO:
+        pass
+
+    #
     transitions = {
         State.UPDATING_CLIENTS: state_updating_clients_transitions,
         State.HOLDING_DOWN: state_holding_down_transitions,
