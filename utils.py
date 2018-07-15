bb2609716f156fd0389a3098d283b3c8de95c7e6
Antoni Przygienda <prz@juniper.net>
2018-07-15 12:52:31 -0700

    two yaml

--- utils.py
+++ utils.py
@@ -1,16 +1,18 @@
 import netifaces
 
 def interface_ipv4_address(interface_name, log = None):
+    defifaddr = '' # "31.133.155.40"
+
     interface_addresses = netifaces.interfaces()
     if not interface_name in netifaces.interfaces():
         if log:
             log.warning('Cannot determine IPv4 address: interface {} does not exists'.format(interface_name))
-        return ''
+        return defifaddr
     interface_addresses = netifaces.ifaddresses(interface_name)
     if not netifaces.AF_INET in interface_addresses:
         if log:
             log.warning('Interface {} does not have an IPv4 address'.format(interface_name))
-        return ''
+        return defifaddr
     return interface_addresses[netifaces.AF_INET][0]['addr']
 
 def system_id_str(system_id):
