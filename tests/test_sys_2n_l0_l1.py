34a284b4e2968f2dae964c241054f84b682f2524
Bruno Rijsman <brunorijsman@gmail.com>
2018-08-02 18:07:49 -0700

    code coverage for system tests

--- tests/test_sys_2n_l0_l1.py
+++ tests/test_sys_2n_l0_l1.py
@@ -5,11 +5,20 @@ import pytest
 EXPECT_TIMEOUT = 3.0
 
 def start_rift(topology, converge_secs=1.0):
-    command_line = "python rift --interactive topology/{}.yaml".format(topology)
+    command_line = ("coverage run --parallel-mode rift --interactive topology/{}.yaml"
+                    .format(topology))
     rift = pexpect.spawn(command_line)
     time.sleep(converge_secs)
     wait_prompt(rift)
     return rift
+
+def stop_rift(rift):
+    # Attempt graceful exit
+    rift.sendline("exit")
+    # Give some time for coverage to write .coverage results file
+    time.sleep(1.0)
+    # Terminate it forcefully, in case the graceful exit did not work for some reason
+    rift.terminate(force=True)
 
 def expect(rift, pattern):
     try:
@@ -29,10 +38,8 @@ def wait_prompt(rift, node_name=None):
         rift.expect("^{}> ".format(node_name))
 
 def test_basic():
-    _rift = start_rift("2n_l0_l1")
-    # expect(rift, "Not getting this")
-    # rift.sendline("show interfaces")
-    # rift.expect("| if1 +| node2-if1 +| 2 +| THREE_WAY +|")
-    # print("Got interface in 3way!")
-    # rift.expect("node1> ")
-    # print("Got prompt!")
+    rift = start_rift("2n_l0_l1")
+    # A very basic test for now: expect 3-way adjacency
+    rift.sendline("show interfaces")
+    rift.expect("| if1 +| node2-if1 +| 2 +| THREE_WAY +|")
+    stop_rift(rift)
