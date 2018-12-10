cd58ae75f84276c173147bd69bd88af3ce75202e
Bruno Rijsman <brunorijsman@gmail.com>
2018-12-10 09:48:39 -0600

    Create ForwardingTable class in fib module

--- rift/fib.py
+++ rift/fib.py
@@ -1,155 +1,49 @@
-# import sortedcontainers
+import sortedcontainers
 
-# import common.ttypes
-# import constants
-# import packet_common
-# import table
+import packet_common
+import route
+import table
 
-# class Route:
+# TODO: If on Linux, and if running in stand-alone mode, program FIB routes into the kernel
 
-#     def __init__(self, prefix, owner, nexthops):
-#         assert isinstance(prefix, common.ttypes.IPPrefixType)
-#         self.prefix = prefix
-#         self.owner = owner
-#         self.nexthops = nexthops
-#         self.stale = False
+class ForwardingTable:
 
-#     @staticmethod
-#     def cli_summary_headers():
-#         return [
-#             "Prefix",
-#             "Owner",
-#             "Nexthops"]
+    def __init__(self, address_family):
+        self.address_family = address_family
+        # Sorted dict of Route objects indexed by prefix. We use the Route class for both the RIB
+        # and the FIB, although not all Route attributes are relevant for the FIB.
+        self.routes = sortedcontainers.SortedDict()
 
-#     def cli_summary_attributes(self):
-#         return [
-#             packet_common.ip_prefix_str(self.prefix),
-#             owner_str(self.owner),
-#             [nexthop_str(nexthop) for nexthop in sorted(self.nexthops)]]
+    def get_route(self, prefix):
+        packet_common.assert_prefix_address_family(prefix, self.address_family)
+        if prefix in self.routes:
+            return self.routes[prefix]
+        else:
+            return None
 
-# class ForwardingTable:
+    def put_route(self, rte):
+        packet_common.assert_prefix_address_family(rte.prefix, self.address_family)
+        self.routes[rte.prefix] = rte
 
-#     def __init__(self, address_family):
-#         self.address_family = address_family
-#         # Sorted dict of _Destination objects indexed by prefix
-#         self.destinations = sortedcontainers.SortedDict()
+    def del_route(self, prefix):
+        # Returns True if the route was present in the table and False if not.
+        packet_common.assert_prefix_address_family(prefix, self.address_family)
+        if prefix in self.routes:
+            del self.routes[prefix]
+            return True
+        else:
+            return False
 
-#     def get_route(self, prefix, owner):
-#         assert_prefix_address_family(prefix, self.address_family)
-#         if prefix in self.destinations:
-#             return self.destinations[prefix].get_route(owner)
-#         else:
-#             return None
+    def all_routes(self):
+        for rte in self.routes:
+            yield rte
 
-#     def put_route(self, route):
-#         assert_prefix_address_family(route.prefix, self.address_family)
-#         route.stale = False
-#         prefix = route.prefix
-#         if route.prefix in self.destinations:
-#             destination = self.destinations[prefix]
-#         else:
-#             destination = _Destination(prefix)
-#             self.destinations[prefix] = destination
-#         destination.put_route(route)
+    def cli_table(self):
+        tab = table.Table()
+        tab.add_row(route.Route.cli_summary_headers())
+        for rte in self.all_routes():
+            tab.add_row(rte.cli_summary_attributes())
+        return tab
 
-#     def del_route(self, prefix, owner):
-#         # Returns True if the route was present in the table and False if not.
-#         assert_prefix_address_family(prefix, self.address_family)
-#         if prefix in self.destinations:
-#             destination = self.destinations[prefix]
-#             deleted = destination.del_route(owner)
-#             if destination.routes == []:
-#                 del self.destinations[prefix]
-#             return deleted
-#         else:
-#             return False
-
-#     def all_routes(self):
-#         for destination in self.destinations.values():
-#             for route in destination.routes:
-#                 yield route
-
-#     def all_prefix_routes(self, prefix):
-#         assert_prefix_address_family(prefix, self.address_family)
-#         if prefix in self.destinations:
-#             destination = self.destinations[prefix]
-#             for route in destination.routes:
-#                 yield route
-#         else:
-#             return None
-
-#     def cli_table(self):
-#         tab = table.Table()
-#         tab.add_row(Route.cli_summary_headers())
-#         for route in self.all_routes():
-#             tab.add_row(route.cli_summary_attributes())
-#         return tab
-
-#     def mark_owner_routes_stale(self, owner):
-#         # Mark all routes of a given owner as stale. Returns number of routes marked.
-#         # A possible more efficient implementation is to have a list of routes for each owner.
-#         # For now, this is good enough.
-#         count = 0
-#         for route in self.all_routes():
-#             if route.owner == owner:
-#                 route.stale = True
-#                 count += 1
-#         return count
-
-#     def del_stale_routes(self):
-#         # Delete all routes still marked as stale. Returns number of deleted routes.
-#         # Cannot delete routes while iterating over routes, so prepare a delete list
-#         routes_to_delete = []
-#         for route in self.all_routes():
-#             if route.stale:
-#                 routes_to_delete.append((route.prefix, route.owner))
-#         # Now delete the routes in the prepared list
-#         count = 0
-#         for (prefix, owner) in routes_to_delete:
-#             self.del_route(prefix, owner)
-#             count += 1
-#         return count
-
-#     def nr_destinations(self):
-#         return len(self.destinations)
-
-#     def nr_routes(self):
-#         count = 0
-#         for destination in self.destinations.values():
-#             count += len(destination.routes)
-#         return count
-
-# class _Destination:
-
-#     def __init__(self, prefix):
-#         self.prefix = prefix
-#         # List of Route objects, in decreasing order or owner (= in decreasing order of
-# preference)
-#         # For a given owner, at most one route is allowed to be in the list
-#         self.routes = []
-
-#     def get_route(self, owner):
-#         for route in self.routes:
-#             if route.owner == owner:
-#                 return route
-#         return None
-
-#     def put_route(self, new_route):
-#         self.del_route(new_route.owner)
-#         index = 0
-#         for existing_route in self.routes:
-#             assert new_route.owner != existing_route.owner
-#             if existing_route.owner < new_route.owner:
-#                 self.routes.insert(index, new_route)
-#                 return
-#             index += 1
-#         self.routes.append(new_route)
-
-#     def del_route(self, owner):
-#         index = 0
-#         for route in self.routes:
-#             if route.owner == owner:
-#                 del self.routes[index]
-#                 return True
-#             index += 1
-#         return False
+    def nr_routes(self):
+        return len(self.routes)
