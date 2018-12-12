072a606af84b752985362d7cb76875282605e176
Bruno Rijsman <brunorijsman@gmail.com>
2018-12-12 14:01:37 -0600

    support ECMP (and NECMP) routes in "show kernel routes"

--- rift/kernel.py
+++ rift/kernel.py
@@ -40,11 +40,11 @@ class Kernel:
         ])
         for addr in self.ipr.get_addr():
             tab.add_row([
-                addr.get_attr('IFA_LABEL'),
-                addr.get_attr('IFA_ADDRESS'),
-                addr.get_attr('IFA_LOCAL'),
-                addr.get_attr('IFA_BROADCAST'),
-                addr.get_attr('IFA_ANYCAST'),
+                self.to_str(addr.get_attr('IFA_LABEL')),
+                self.to_str(addr.get_attr('IFA_ADDRESS')),
+                self.to_str(addr.get_attr('IFA_LOCAL')),
+                self.to_str(addr.get_attr('IFA_BROADCAST')),
+                self.to_str(addr.get_attr('IFA_ANYCAST')),
             ])
         cli_session.print("Kernel Addresses:")
         cli_session.print(tab.to_string())
@@ -64,12 +64,12 @@ class Kernel:
         ])
         for link in self.ipr.get_links():
             tab.add_row([
-                link.get_attr('IFLA_IFNAME'),
-                link["index"],
-                link.get_attr('IFLA_ADDRESS'),
-                link.get_attr('IFLA_BROADCAST'),
-                link.get_attr('IFLA_LINK'),
-                link.get_attr('IFLA_MTU'),
+                self.to_str(link.get_attr('IFLA_IFNAME')),
+                self.to_str(link["index"]),
+                self.to_str(link.get_attr('IFLA_ADDRESS')),
+                self.to_str(link.get_attr('IFLA_BROADCAST')),
+                self.to_str(link.get_attr('IFLA_LINK')),
+                self.to_str(link.get_attr('IFLA_MTU')),
                 self.link_flags_to_str(link["flags"]),
             ])
         cli_session.print("Kernel Links:")
@@ -193,7 +193,56 @@ class Kernel:
                 prefix_str += "/" + str(src_len)
         return prefix_str
 
+    @staticmethod
+    def kernel_route_multipath_nhops(route, links):
+        # Returns a list of three-tuples: [(oif_str, gateway_str, weight_str)]
+        multipath_list = route.get_attr('RTA_MULTIPATH')
+        if multipath_list is None:
+            return []
+        next_hops_list = []
+        for path in multipath_list:
+            # TODO: Add support for path["flags"].
+            if "oif" in path:
+                oif_index = path["oif"]
+                oif_str = Kernel.interface_index_to_name(oif_index, links)
+            else:
+                oif_str = ""
+            if "attrs" in path:
+                attr_list = path["attrs"]
+            else:
+                attr_list = []
+            gateway_str = ""
+            for (attr_name, attr_value) in attr_list:
+                if attr_name == "RTA_GATEWAY":
+                    gateway_str = attr_value
+                    break
+            if "hops" in path:
+                weight_str = path["hops"]
+            else:
+                weight_str = ""
+            next_hop = (oif_str, gateway_str, weight_str)
+            next_hops_list.append(next_hop)
+        return next_hops_list
+
+    @staticmethod
+    def kernel_route_nhops(route, links):
+        oif = route.get_attr('RTA_OIF')
+        multipath = route.get_attr('RTA_MULTIPATH')
+        assert (oif is None) or (multipath is None)  # Can't have OIF and MULTIPATH
+        if oif is not None:
+            oif_str = Kernel.interface_index_to_name(oif, links)
+            gateway_str = Kernel.to_str(route.get_attr('RTA_GATEWAY'))
+            weight_str = ""
+            next_hops = [(oif_str, gateway_str, weight_str)]
+        elif multipath is not None:
+            next_hops = Kernel.kernel_route_multipath_nhops(route, links)
+        else:
+            next_hops = []
+        return next_hops
+
+
     def command_show_routes(self, cli_session, table_nr):
+        # pylint:disable=too-many-locals
         if self.unsupported_platform_error(cli_session):
             return
         links = self.ipr.get_links()
@@ -201,40 +250,51 @@ class Kernel:
         for route in self.ipr.get_routes():
             family = route["family"]
             dst_prefix_str = self.kernel_route_dst_prefix_str(route)
-            outgoing_interface_name = self.interface_index_to_name(route.get_attr('RTA_OIF'), links)
             route_table_nr = route.get_attr('RTA_TABLE')
             if (table_nr is not None) and (table_nr != route_table_nr):
                 continue
-            rows.append([
-                route_table_nr,
-                self.af_str(family),
-                dst_prefix_str,
-                self.route_type_str(route["type"]),
-                self.proto_str(route["proto"]),
-                outgoing_interface_name,
-                self.to_str(route.get_attr('RTA_GATEWAY'))
-            ])
+            next_hops = self.kernel_route_nhops(route, links)
+            oif_cell = []
+            gateway_cell = []
+            weight_cell = []
+            for next_hop in next_hops:
+                oif_cell.append(next_hop[0])
+                gateway_cell.append(next_hop[1])
+                weight_cell.append(next_hop[2])
+            rows.append([route_table_nr,
+                         self.af_str(family),
+                         dst_prefix_str,
+                         self.route_type_str(route["type"]),
+                         self.proto_str(route["proto"]),
+                         oif_cell,
+                         gateway_cell,
+                         weight_cell])
         rows.sort(key=self.route_row_key)
         # Now that the output is sorted, replace number numbers with symbolic names
         for row in rows:
             row[0] = self.table_nr_to_name(row[0])
         tab = table.Table()
-        tab.add_row([
-            "Table",
-            ["Address", "Family"],
-            "Destination",
-            "Type",
-            "Protocol",
-            ["Outgoing", "Interface"],
-            "Gateway"
-        ])
+        tab.add_row(["Table",
+                     ["Address", "Family"],
+                     "Destination",
+                     "Type",
+                     "Protocol",
+                     ["Outgoing", "Interface"],
+                     "Gateway",
+                     "Weight"])
         tab.add_rows(rows)
         cli_session.print("Kernel Routes:")
         cli_session.print(tab.to_string())
 
     def command_show_route_prefix(self, cli_session, table_nr, prefix):
-        # TODO: Report fields (similar to index and flags in links):
-        # src_len, tos, scope, flags
+        # TODO: Add support for:
+        # - Attribute RTA_FLOW
+        # - Attribute RTA_ENCAP_TYPE
+        # - Attribute RTA_ENCAP
+        # - Attribute RTA_METRICS
+        # - Field tos
+        # - Field flags
+        # - Field scope
         if self.unsupported_platform_error(cli_session):
             return
         route = None
@@ -249,15 +309,18 @@ class Kernel:
                               .format(prefix, self.table_nr_to_name(table_nr)))
             return
         links = self.ipr.get_links()
-        outgoing_interface_name = self.interface_index_to_name(route.get_attr('RTA_OIF'), links)
         tab = table.Table(separators=False)
+        next_hops = self.kernel_route_nhops(route, links)
+        next_hops_cell = []
+        for next_hop in next_hops:
+            next_hop_str = str(next_hop[0]) + " " + str(next_hop[1]) + " " + str(next_hop[2])
+            next_hops_cell.append(next_hop_str)
         tab.add_row(["Table", self.table_nr_to_name(table_nr)])
         tab.add_row(["Address Family", self.af_str(self.af_str(route["family"]))])
         tab.add_row(["Destination", str(prefix)])
         tab.add_row(["Type", self.route_type_str(route["type"])])
         tab.add_row(["Protocol", self.proto_str(route["proto"])])
-        tab.add_row(["Outgoing Interface", outgoing_interface_name])
-        tab.add_row(["Gateway", self.to_str(route.get_attr('RTA_GATEWAY'))])
+        tab.add_row(["Next-hops", next_hops_cell])
         tab.add_row(["Priority", self.to_str(route.get_attr('RTA_PRIORITY'))])
         tab.add_row(["Preference", self.to_str(route.get_attr('RTA_PREF'))])
         tab.add_row(["Preferred Source Address", self.to_str(route.get_attr('RTA_PREFSRC'))])
