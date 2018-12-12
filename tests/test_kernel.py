9a13eeb3e133266b5808cb91981f9f4a39a4e6a3
Bruno Rijsman <brunorijsman@gmail.com>
2018-12-12 15:27:22 -0600

    Add test_show_kernel_links

--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -1,7 +1,26 @@
+import re
+
 import kernel
 
-def test_unsupported_platform_error():
+# pylint: disable=line-too-long
+# pylint: disable=bad-continuation
+
+def test_show_kernel_links():
     kern = kernel.Kernel()
-    assert kern.platform_supported
+    if not kern.platform_supported:
+        return
+    # We assume the loopback interface (lo) is always there, and we look for something like this:
+    # +-----------+-----------+-------------------+-------------------+-----------+-------+-----------+
+    # | Interface | Interface | Hardware          | Hardware          | Link Type | MTU   | Flags     |
+    # | Name      | Index     | Address           | Broadcast         |           |       |           |
+    # |           |           |                   | Address           |           |       |           |
+    # +-----------+-----------+-------------------+-------------------+-----------+-------+-----------+
+    # | lo        | 1         | 00:00:00:00:00:00 | 00:00:00:00:00:00 |           | 65536 | UP        |
+    # ...
     tab_str = kern.cli_links_table().to_string()
-    assert tab_str == "foo"
+    pattern = (r"[|] Interface +[|] +Interface +[|] +Hardware +[|] +Hardware  +[|] +Link Type +[|] +MTU   +[|] +Flags +[|]\n"
+                "[|] Name      +[|] +Index     +[|] +Address  +[|] +Broadcast +[|] +          +[|] +      +[|] +      +[|]\n"
+                "[|]           +[|] +          +[|] +         +[|] +Address   +[|] +          +[|] +      +[|] +      +[|]\n"
+                "(.|[\n])*"
+                "[|] lo +[|] +[0-9]+ +[|] +[0-9:]+ +[|] +[0-9:]+ +[|] + +[|] +[0-9 ]+[|] +UP +[|]\n")
+    assert re.search(pattern, tab_str) is not None
