5108d37cfa6f44f23c99969e590a4e8afc5b7d7b
Bruno Rijsman <brunorijsman@gmail.com>
2018-12-12 15:49:30 -0600

    Check networking on codecov

---
+++ tests/test_real_topology.py
@@ -0,0 +1,21 @@
+import platform
+import subprocess
+
+def test_ls():
+    assert subprocess.call(["ls"]) == 0
+
+def test_ip_route():
+    #assert platform.system() == "Linux"
+    assert subprocess.call(["ip", "route"]) == 0
+
+def test_brctl():
+    #assert platform.system() == "Linux"
+    assert subprocess.call(["brctl"]) == 0
+
+def test_brctl_show():
+    #assert platform.system() == "Linux"
+    assert subprocess.call(["brctl", "show"]) == 0
+
+def test_brctl_add():
+    #assert platform.system() == "Linux"
+    assert subprocess.call(["brctl", "addbr", "foo"]) == 0
