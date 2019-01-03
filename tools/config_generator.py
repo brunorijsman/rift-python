4574f69e98632cfc4c455787563852e183628484
Bruno Rijsman <brunorijsman@gmail.com>
2019-01-03 16:58:09 -0600

    Meta-topology: Superspine nodes and links (single plane), Generalize PODs and planes to groups, Superspine graphics

--- tools/config_generator.py
+++ tools/config_generator.py
@@ -25,6 +25,7 @@ SCHEMA = {
     'nr-leaf-nodes-per-pod': {'required': True, 'type': 'integer', 'min': 1},
     'nr-pods': {'required': False, 'type': 'integer', 'min': 1, 'default': 1},
     'nr-spine-nodes-per-pod': {'required': True, 'type': 'integer', 'min': 1},
+    'nr-superspine-nodes': {'required': False, 'type': 'integer', 'min': 1},
     'leafs': NODE_SCHEMA,
     'spines': NODE_SCHEMA
 }
@@ -32,15 +33,20 @@ SCHEMA = {
 SOUTH = 1
 NORTH = 2
 
+LEAF_LEVEL = 0
+SPINE_LEVEL = 1
+SUPERSPINE_LEVEL = 2
+
 GLOBAL_X_OFFSET = 10
 GLOBAL_Y_OFFSET = 10
-POD_X_INTERVAL = 30
-POD_X_SPACER = 24
-POD_Y_SPACER = 24
-POD_X_TEXT_OFFSET = 8
-POD_Y_TEXT_OFFSET = 16
-POD_LINE_COLOR = "black"
-POD_FILL_COLOR = "#F0F0F0"
+SUPERSPINE_TO_POD_Y_INTERVAL = 140
+GROUP_X_INTERVAL = 30
+GROUP_X_SPACER = 24
+GROUP_Y_SPACER = 24
+GROUP_X_TEXT_OFFSET = 8
+GROUP_Y_TEXT_OFFSET = 16
+GROUP_LINE_COLOR = "black"
+GROUP_FILL_COLOR = "#F0F0F0"
 NODE_X_SIZE = 100
 NODE_Y_SIZE = 50
 NODE_X_INTERVAL = 20
@@ -139,79 +145,58 @@ def generate_ipv4_address_str(byte1, byte2, byte3, byte4, offset):
     ip_address_str = "{}.{}.{}.{}".format(byte1, byte2, byte3, byte4)
     return ip_address_str
 
-class Pod:
+class Group:
 
-    next_pod_id = 1
-
-    def __init__(self, name, only_pod):
+    def __init__(self, fabric, name, group_index, only_instance, y_pos, y_size):
+        self.fabric = fabric
         self.name = name
-        self.only_pod = only_pod
-        self.pod_id = Pod.next_pod_id
-        Pod.next_pod_id += 1
-        self.nr_leaf_nodes = META_CONFIG['nr-leaf-nodes-per-pod']
-        if 'leafs' in META_CONFIG:
-            self.leaf_nr_ipv4_loopbacks = META_CONFIG['leafs']['nr-ipv4-loopbacks']
-        else:
-            self.leaf_nr_ipv4_loopbacks = DEFAULT_NR_IPV4_LOOPBACKS
-        self.nr_spine_nodes = META_CONFIG['nr-spine-nodes-per-pod']
-        if 'spines' in META_CONFIG:
-            self.spine_nr_ipv4_loopbacks = META_CONFIG['spines']['nr-ipv4-loopbacks']
-        else:
-            self.spine_nr_ipv4_loopbacks = DEFAULT_NR_IPV4_LOOPBACKS
-        self.nodes = {}
-        self.leaf_nodes = []
-        self.spine_nodes = []
-        self.leaf_spine_links = []
-        self.create_leaf_nodes()
-        self.create_spine_nodes()
-        self.create_leaf_spine_links()
+        self.group_index = group_index
+        self.only_instance = only_instance
+        self.given_y_pos = y_pos
+        self.given_y_size = y_size
+        self.x_center_shift = 0
+        self.nodes = []
+        self.nodes_by_level = {}
+        self.links = []
+
+    def create_node(self, name, level, index_in_level, y_pos):
+        node = Node(self, name, level, index_in_level, y_pos)
+        # TODO: Move adding of loopbacks to here
+        self.nodes.append(node)
+        if level not in self.nodes_by_level:
+            self.nodes_by_level[level] = []
+        self.nodes_by_level[level].append(node)
+        return node
+
+    def create_link(self, from_node, to_node):
+        link = Link(from_node, to_node)
+        self.links.append(link)
+        return link
 
     def node_name(self, base_name, node_index_in_level):
-        if self.only_pod:
+        if self.only_instance:
             return base_name + "-" + str(node_index_in_level + 1)
         else:
-            return base_name + "-" + str(self.pod_id) + "-" + str(node_index_in_level + 1)
-
-    def create_leaf_nodes(self):
-        for index in range(0, self.nr_leaf_nodes):
-            node_name = self.node_name("leaf", index)
-            node = Node(self, node_name, level=0, index_in_level=index)
-            node.add_ipv4_loopbacks(self.leaf_nr_ipv4_loopbacks)
-            self.nodes[node.node_id] = node
-            self.leaf_nodes.append(node)
-
-    def create_spine_nodes(self):
-        for index in range(0, self.nr_spine_nodes):
-            node_name = self.node_name("spine", index)
-            node = Node(self, node_name, level=1, index_in_level=index)
-            node.add_ipv4_loopbacks(self.spine_nr_ipv4_loopbacks)
-            self.nodes[node.node_id] = node
-            self.spine_nodes.append(node)
-
-    def create_leaf_spine_links(self):
-        for leaf_node in self.leaf_nodes:
-            for spine_node in self.spine_nodes:
-                link = Link(leaf_node, spine_node)
-                self.leaf_spine_links.append(link)
+            return base_name + "-" + str(self.group_index) + "-" + str(node_index_in_level + 1)
 
     def write_config_to_file(self, file, netns):
-        for node in self.nodes.values():
+        for node in self.nodes:
             node.write_config_to_file(file, netns)
 
     def write_netns_configs_and_scripts(self):
-        for node in self.nodes.values():
+        for node in self.nodes:
             node.write_netns_configs_and_scripts()
 
+    # TODO: Split this in phase1 and phase2 at a higher level
     def write_netns_start_scr_to_file(self, file):
-        for link in self.leaf_spine_links:
+        for link in self.links:
             link.write_netns_start_scr_to_file(file)
-        for node in self.nodes.values():
+        for node in self.nodes:
             node.write_netns_start_scr_to_file_1(file)
-        for node in self.nodes.values():
+        for node in self.nodes:
             node.write_netns_start_scr_to_file_2(file)
 
     def write_graphics_to_file(self, file):
-
         x_pos = self.x_pos()
         y_pos = self.y_pos()
         x_size = self.x_size()
@@ -224,63 +209,136 @@ class Pod:
                    'fill="{}" '
                    'style="stroke:{}" '
                    '></rect>'
-                   .format(x_pos, y_pos, x_size, y_size, POD_FILL_COLOR, POD_LINE_COLOR))
-        if not self.only_pod:
-            x_pos += POD_X_TEXT_OFFSET
-            y_pos += POD_Y_TEXT_OFFSET
+                   .format(x_pos, y_pos, x_size, y_size, GROUP_FILL_COLOR, GROUP_LINE_COLOR))
+        if not self.only_instance:
+            x_pos += GROUP_X_TEXT_OFFSET
+            y_pos += GROUP_Y_TEXT_OFFSET
             file.write('<text '
                        'x="{}" '
                        'y="{}" '
                        'style="font-family:monospace;stroke:{}">'
                        '{}'
                        '</text>\n'
-                       .format(x_pos, y_pos, POD_LINE_COLOR, self.name))
-        for node in self.nodes.values():
+                       .format(x_pos, y_pos, GROUP_LINE_COLOR, self.name))
+        for node in self.nodes:
             node.write_graphics_to_file(file)
-        for link in self.leaf_spine_links:
+        for link in self.links:
             link.write_graphics_to_file(file)
 
     def x_pos(self):
-        # X position of top-left corner of rectangle representing the pod
-        return GLOBAL_X_OFFSET + (self.pod_id - 1) * (self.x_size() + POD_X_INTERVAL)
-
-    def x_size(self):
-        width_nodes = max(self.nr_leaf_nodes, self.nr_spine_nodes)
-        width = width_nodes * NODE_X_SIZE
-        width += (width_nodes - 1) * NODE_X_INTERVAL
-        width += 2 * POD_X_SPACER
-        return width
+        # X position of top-left corner of rectangle representing the group
+        x_pos = GLOBAL_X_OFFSET
+        x_pos += self.group_index * (self.x_size() + GROUP_X_INTERVAL)
+        x_pos += self.x_center_shift
+        return x_pos
 
     def y_pos(self):
-        # Y position of top-left corner of rectangle representing the pod
-        return GLOBAL_Y_OFFSET
+        return self.given_y_pos
+
+    def nr_nodes_in_widest_level(self):
+        result = 0
+        for nodes_in_level_list in self.nodes_by_level.values():
+            nr_nodes_in_level = len(nodes_in_level_list)
+            result = max(result, nr_nodes_in_level)
+        return result
+
+    def x_size(self):
+        width_nodes = self.nr_nodes_in_widest_level()
+        width = width_nodes * NODE_X_SIZE
+        width += (width_nodes - 1) * NODE_X_INTERVAL
+        width += 2 * GROUP_X_SPACER   # TODO: Change to GROUP_X_SPACER
+        return width
 
     def y_size(self):
-        height = 2 * NODE_Y_SIZE
-        height += NODE_Y_INTERVAL
-        height += 2 * POD_Y_SPACER
-        return height
+        return self.given_y_size
 
     def first_node_x_pos(self, level):
-        width_nodes = max(self.nr_leaf_nodes, self.nr_spine_nodes)
-        if level == 0:
-            missing_nodes = width_nodes - self.nr_leaf_nodes
-        else:
-            missing_nodes = width_nodes - self.nr_spine_nodes
+        width_nodes = self.nr_nodes_in_widest_level()
+        missing_nodes = width_nodes - len(self.nodes_by_level[level])
         padding_width = missing_nodes * (NODE_X_SIZE + NODE_X_INTERVAL) // 2
-        return self.x_pos() + padding_width + POD_X_SPACER
+        return self.x_pos() + padding_width + GROUP_X_SPACER
+
+    def nr_levels(self):
+        return len(self.nodes_by_level)
+
+class Pod(Group):
+
+    def __init__(self, fabric, pod_name, pod_index, only_pod, y_pos):
+        y_size = 2 * NODE_Y_SIZE + NODE_Y_INTERVAL + 2 * GROUP_Y_SPACER
+        Group.__init__(self, fabric, pod_name, pod_index, only_pod, y_pos, y_size)
+        self.leaf_nodes = []
+        self.spine_nodes = []
+        self.nr_leaf_nodes = META_CONFIG['nr-leaf-nodes-per-pod']
+        # TODO: Make nr-ipv4-loopbacks a global knob
+        if 'leafs' in META_CONFIG:
+            self.leaf_nr_ipv4_loopbacks = META_CONFIG['leafs']['nr-ipv4-loopbacks']
+        else:
+            self.leaf_nr_ipv4_loopbacks = DEFAULT_NR_IPV4_LOOPBACKS
+        self.nr_spine_nodes = META_CONFIG['nr-spine-nodes-per-pod']
+        if 'spines' in META_CONFIG:
+            self.spine_nr_ipv4_loopbacks = META_CONFIG['spines']['nr-ipv4-loopbacks']
+        else:
+            self.spine_nr_ipv4_loopbacks = DEFAULT_NR_IPV4_LOOPBACKS
+        self.nr_superspine_nodes = 0
+        self.create_leaf_nodes()
+        self.create_spine_nodes()
+        self.create_leaf_spine_links()
+
+    def create_leaf_nodes(self):
+        y_pos = self.y_pos() + GROUP_Y_SPACER + NODE_Y_SIZE + NODE_Y_INTERVAL
+        for index in range(0, self.nr_leaf_nodes):
+            node_name = self.node_name("leaf", index)
+            node = self.create_node(node_name, LEAF_LEVEL, index, y_pos)
+            node.add_ipv4_loopbacks(self.leaf_nr_ipv4_loopbacks)
+            self.leaf_nodes.append(node)
+
+    def create_spine_nodes(self):
+        y_pos = self.y_pos() + GROUP_Y_SPACER
+        for index in range(0, self.nr_spine_nodes):
+            node_name = self.node_name("spine", index)
+            node = self.create_node(node_name, SPINE_LEVEL, index, y_pos)
+            node.add_ipv4_loopbacks(self.spine_nr_ipv4_loopbacks)
+            self.spine_nodes.append(node)
+
+    def create_leaf_spine_links(self):
+        for leaf_node in self.leaf_nodes:
+            for spine_node in self.spine_nodes:
+                _link = self.create_link(leaf_node, spine_node)
+
+class Plane(Group):
+
+    def __init__(self, fabric, plane_name, plane_index, only_plane, y_pos):
+        y_size = NODE_Y_SIZE + 2 * GROUP_Y_SPACER
+        Group.__init__(self, fabric, plane_name, plane_index, only_plane, y_pos, y_size)
+        self.superspine_nodes = []
+        self.superspine_spine_links = []
+        self.nr_leaf_nodes = 0
+        self.nr_spine_nodes = 0
+        self.nr_superspine_nodes = META_CONFIG['nr-superspine-nodes']
+        # TODO: Make nr-ipv4-loopbacks a global knob
+        self.superspine_nr_ipv4_loopbacks = 1
+        self.create_superspine_nodes()
+
+    def create_superspine_nodes(self):
+        y_pos = self.y_pos() + GROUP_Y_SPACER
+        for index in range(0, self.nr_superspine_nodes):
+            node_name = self.node_name("super", index)   # TODO: Index per group type
+            node = self.create_node(node_name, SUPERSPINE_LEVEL, index, y_pos)
+            node.add_ipv4_loopbacks(self.superspine_nr_ipv4_loopbacks)
+            self.superspine_nodes.append(node)
 
 class Node:
 
     next_node_id = 1
 
-    def __init__(self, pod, name, level, index_in_level):
-        self.pod = pod
+    def __init__(self, group, name, level, index_in_level, y_pos):
+        self.group = group
         self.name = name
         self.node_id = Node.next_node_id
         Node.next_node_id += 1
         self.level = level
         self.index_in_level = index_in_level
+        self.given_y_pos = y_pos
         self.rx_lie_mcast_addr = generate_ipv4_address_str(224, 0, 1, 0, self.node_id)
         self.interfaces = []
         self.ipv4_prefixes = []
@@ -434,13 +492,11 @@ class Node:
     def x_pos(self):
         # X position of top-left corner of rectangle representing the node
         x_delta = self.index_in_level * (NODE_X_SIZE + NODE_X_INTERVAL)
-        return self.pod.first_node_x_pos(self.level) + x_delta
+        return self.group.first_node_x_pos(self.level) + x_delta
 
     def y_pos(self):
         # Y position of top-left corner of rectangle representing the node
-        nr_levels = 2
-        y_delta = (nr_levels - self.level - 1) * (NODE_Y_SIZE + NODE_Y_INTERVAL)
-        return self.pod.y_pos() + POD_Y_SPACER + y_delta
+        return self.given_y_pos
 
 class Interface:
 
@@ -542,27 +598,49 @@ class Link:
         self.intf2.write_graphics_to_file(file)
         file.write('</g>\n')
 
-class Generator:
+class Fabric:
 
     def __init__(self):
         self.nr_pods = META_CONFIG['nr-pods']
         self.pods = []
-
-    def run(self):
-        self.generate_representation()
-        if ARGS.netns_per_node:
-            self.write_netns_configs_and_scripts()
-        else:
-            self.write_config()
-        if ARGS.graphics_file is not None:
-            self.write_graphics()
-
-    def generate_representation(self):
+        self.planes = []
+        pods_y_pos = GLOBAL_Y_OFFSET
+        # Only generate superspine nodes and planes if there is more than one pod
+        if self.nr_pods > 1:
+            nr_planes = 1   # TODO: Implement multi-plane
+            only_plane = (nr_planes == 1)
+            planes_y_pos = GLOBAL_Y_OFFSET
+            pods_y_pos += NODE_Y_SIZE + SUPERSPINE_TO_POD_Y_INTERVAL
+            for index in range(0, nr_planes):
+                plane_name = "plane-" + str(index + 1)
+                pod = Plane(self, plane_name, index, only_plane, planes_y_pos)
+                self.planes.append(pod)
+        # Generate the pods with leaf and spine nodes within them
         only_pod = (self.nr_pods == 1)
         for index in range(0, self.nr_pods):
             pod_name = "pod-" + str(index + 1)
-            pod = Pod(pod_name, only_pod)
+            pod = Pod(self, pod_name, index, only_pod, pods_y_pos)
             self.pods.append(pod)
+        # Center the pods and planes
+        pod_x_center_shift = (self.x_size() - self.pods_total_x_size()) // 2
+        for pod in self.pods:
+            pod.x_center_shift = pod_x_center_shift
+        plane_x_center_shift = (self.x_size() - self.planes_total_x_size()) // 2
+        for plane in self.planes:
+            plane.x_center_shift = plane_x_center_shift
+        # Generate the links between the superspine nodes and the spine nodes
+        if self.planes:
+            # TODO: Add support for multi-plane
+            self.create_links_single_plane()
+
+    def create_links_single_plane(self):
+        # Superspine to spine links (single plane)
+        assert len(self.planes) == 1
+        plane = self.planes[0]
+        for superspine_node in plane.nodes:
+            for pod in self.pods:
+                for spine_node in pod.nodes_by_level[SPINE_LEVEL]:
+                    _link = plane.create_link(superspine_node, spine_node)
 
     def write_config(self):
         file_name = getattr(ARGS, 'output-file-or-dir')
@@ -630,6 +708,8 @@ class Generator:
         self.svg_start(file)
         for pod in self.pods:
             pod.write_graphics_to_file(file)
+        for plane in self.planes:
+            plane.write_graphics_to_file(file)
         self.svg_end(file)
 
     def svg_start(self, file):
@@ -642,6 +722,25 @@ class Generator:
 
     def svg_end(self, file):
         file.write(END_OF_SVG)
+
+    def pods_total_x_size(self):
+        total_x_size = 0
+        for pod in self.pods:
+            total_x_size += pod.x_size() + GROUP_X_INTERVAL
+        if total_x_size > 0:
+            total_x_size -= GROUP_X_INTERVAL
+        return total_x_size
+
+    def planes_total_x_size(self):
+        total_x_size = 0
+        for plane in self.planes:
+            total_x_size += plane.x_size() + GROUP_X_INTERVAL
+        if total_x_size > 0:
+            total_x_size -= GROUP_X_INTERVAL
+        return total_x_size
+
+    def x_size(self):
+        return max(self.pods_total_x_size(), self.planes_total_x_size())
 
 def parse_meta_configuration(file_name):
     try:
@@ -659,8 +758,19 @@ def parse_meta_configuration(file_name):
         exit(1)
     return validator.normalized(config)
 
+def validate_meta_configuration():
+    nr_pods = META_CONFIG['nr-pods']
+    if 'nr-superspine-nodes' in META_CONFIG:
+        nr_superspine_nodes = META_CONFIG['nr-superspine-nodes']
+    else:
+        nr_superspine_nodes = None
+    if (nr_pods > 1) and (nr_superspine_nodes is None):
+        fatal_error("nr-superspine-nodes must be configured if number of PODs > 1")
+    if (nr_pods == 1) and (nr_superspine_nodes is not None):
+        fatal_error("nr-superspine-nodes must not be configured if there is only one POD")
+
 def parse_command_line_arguments():
-    parser = argparse.ArgumentParser(description='RIFT configuration generator')
+    parser = argparse.ArgumentParser(description='RIFT configuration fabric')
     parser.add_argument(
         'input-meta-config-file',
         help='Input meta-configuration file name')
@@ -689,8 +799,14 @@ def main():
     ARGS = parse_command_line_arguments()
     input_file_name = getattr(ARGS, 'input-meta-config-file')
     META_CONFIG = parse_meta_configuration(input_file_name)
-    generator = Generator()
-    generator.run()
+    validate_meta_configuration()
+    fabric = Fabric()
+    if ARGS.netns_per_node:
+        fabric.write_netns_configs_and_scripts()
+    else:
+        fabric.write_config()
+    if ARGS.graphics_file is not None:
+        fabric.write_graphics()
 
 if __name__ == "__main__":
     main()
