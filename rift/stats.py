caeeafde911b66779d072441883bb0a6be67d0dc
Bruno Rijsman <brunorijsman@gmail.com>
2019-02-17 22:00:38 -0300

    Introduce multi-counter

--- rift/stats.py
+++ rift/stats.py
@@ -1,3 +1,5 @@
+import operator
+
 import table
 
 class Group:
@@ -12,8 +14,8 @@ class Group:
         tab = table.Table()
         tab.add_row(["Description", "Value"])
         for member in self._members:
-            if not exclude_zero or member.value:
-                tab.add_row([member.description, member.value_with_units])
+            if not exclude_zero or not member.is_zero():
+                tab.add_row([member.description(), member.value_display_str()])
         return tab
 
 class Counter:
@@ -26,26 +28,65 @@ class Counter:
             self._unit_plural = unit_singular + 's'
         else:
             self._unit_plural = unit_plural
-        self._count = 0
+        self._value = 0
         group.add_member(self)
 
-    def add(self, value):
-        self._count += value
+    def add(self, add_value):
+        self._value += add_value
 
     def increase(self):
-        self.add(1)
+        self._value += 1
 
-    @property
     def description(self):
         return self._description
 
-    @property
-    def value(self):
-        return self._count
+    def is_zero(self):
+        return self._value == 0
 
-    @property
-    def value_with_units(self):
-        if self._count == 1:
-            return str(self._count) + ' ' + self._unit_singular
+    def value_display_str(self):
+        if self._value == 1:
+            return str(self._value) + ' ' + self._unit_singular
         else:
-            return str(self._count) + ' ' + self._unit_plural
+            return str(self._value) + ' ' + self._unit_plural
+
+class MultiCounter:
+
+    def __init__(self, group, description, units_singular, units_plural=None):
+        assert isinstance(units_singular, list)
+        if units_plural is None:
+            units_plural = list(map(lambda word: word + 's', units_singular))
+        assert isinstance(units_plural, list)
+        assert len(units_singular) == len(units_plural)
+        self._group = group
+        self._description = description
+        self._units_singular = units_singular
+        self._units_plural = units_plural
+        self._nr_values = len(units_singular)
+        self._values = []
+        for _index in range(0, self._nr_values):
+            self._values.append(0)
+        group.add_member(self)
+
+    def add(self, add_values):
+        assert isinstance(add_values, list)
+        assert len(add_values) == len(self._values)
+        self._values = list(map(operator.add, self._values, add_values))
+
+    def description(self):
+        return self._description
+
+    def is_zero(self):
+        for count in self._values:
+            if count:
+                return False
+        return True
+
+    def value_display_str(self):
+        value_strs = []
+        for index, value in enumerate(self._values):
+            if value == 1:
+                value_str = str(value) + ' ' + self._units_singular[index]
+            else:
+                value_str = str(value) + ' ' + self._units_plural[index]
+            value_strs.append(value_str)
+        return ", ".join(value_strs)
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -7,44 +7,81 @@ def test_group():
     _counter_2 = stats.Counter(group, "Received Packets", "Packet")
     _counter_3 = stats.Counter(group, "Dropped Packets", "Packet")
     _counter_3.add(5)
+    _counter_4 = stats.MultiCounter(group, "Sent Hellos", ["Packet", "Byte"])
+    _counter_5 = stats.MultiCounter(group, "Received Hellos", ["Packet", "Byte"])
+    _counter_5.add([2, 33])
     assert group.table(exclude_zero=False).to_string() == (
-        "+------------------+-----------+\n"
-        "| Description      | Value     |\n"
-        "+------------------+-----------+\n"
-        "| Sent Packets     | 1 Packet  |\n"
-        "+------------------+-----------+\n"
-        "| Received Packets | 0 Packets |\n"
-        "+------------------+-----------+\n"
-        "| Dropped Packets  | 5 Packets |\n"
-        "+------------------+-----------+\n")
+        "+------------------+---------------------+\n"
+        "| Description      | Value               |\n"
+        "+------------------+---------------------+\n"
+        "| Sent Packets     | 1 Packet            |\n"
+        "+------------------+---------------------+\n"
+        "| Received Packets | 0 Packets           |\n"
+        "+------------------+---------------------+\n"
+        "| Dropped Packets  | 5 Packets           |\n"
+        "+------------------+---------------------+\n"
+        "| Sent Hellos      | 0 Packets, 0 Bytes  |\n"
+        "+------------------+---------------------+\n"
+        "| Received Hellos  | 2 Packets, 33 Bytes |\n"
+        "+------------------+---------------------+\n")
     assert group.table(exclude_zero=True).to_string() == (
-        "+-----------------+-----------+\n"
-        "| Description     | Value     |\n"
-        "+-----------------+-----------+\n"
-        "| Sent Packets    | 1 Packet  |\n"
-        "+-----------------+-----------+\n"
-        "| Dropped Packets | 5 Packets |\n"
-        "+-----------------+-----------+\n")
+        "+-----------------+---------------------+\n"
+        "| Description     | Value               |\n"
+        "+-----------------+---------------------+\n"
+        "| Sent Packets    | 1 Packet            |\n"
+        "+-----------------+---------------------+\n"
+        "| Dropped Packets | 5 Packets           |\n"
+        "+-----------------+---------------------+\n"
+        "| Received Hellos | 2 Packets, 33 Bytes |\n"
+        "+-----------------+---------------------+\n")
 
 def test_counter():
     group = stats.Group()
     counter_1 = stats.Counter(group, "RX TIE Packets", "Packet")
-    assert counter_1.description == "RX TIE Packets"
-    assert counter_1.value == 0
-    assert counter_1.value_with_units == "0 Packets"
+    assert counter_1.description() == "RX TIE Packets"
+    assert counter_1.value_display_str() == "0 Packets"
+    assert counter_1.is_zero()
     counter_1.increase()
-    assert counter_1.value == 1
-    assert counter_1.value_with_units == "1 Packet"
+    assert counter_1.value_display_str() == "1 Packet"
+    assert not counter_1.is_zero()
     counter_1.increase()
-    assert counter_1.value == 2
-    assert counter_1.value_with_units == "2 Packets"
+    assert counter_1.value_display_str() == "2 Packets"
+    assert not counter_1.is_zero()
     counter_1.add(7)
-    assert counter_1.value == 9
-    assert counter_1.value_with_units == "9 Packets"
+    assert counter_1.value_display_str() == "9 Packets"
+    assert not counter_1.is_zero()
     counter_2 = stats.Counter(group, "Caught Fish", "Fish", "Fish")
-    assert counter_2.description == "Caught Fish"
-    assert counter_2.value == 0
-    assert counter_2.value_with_units == "0 Fish"
+    assert counter_2.description() == "Caught Fish"
+    assert counter_2.value_display_str() == "0 Fish"
+    assert counter_2.is_zero()
     counter_2.increase()
-    assert counter_2.value == 1
-    assert counter_2.value_with_units == "1 Fish"
+    assert counter_2.value_display_str() == "1 Fish"
+    assert not counter_2.is_zero()
+
+def test_multi_counter():
+    group = stats.Group()
+    counter_1 = stats.MultiCounter(group, "RX TIE Packets", ["Packet", "Byte"])
+    assert counter_1.description() == "RX TIE Packets"
+    assert counter_1.value_display_str() == "0 Packets, 0 Bytes"
+    assert counter_1.is_zero()
+    counter_1.add([1, 0])
+    assert counter_1.value_display_str() == "1 Packet, 0 Bytes"
+    assert not counter_1.is_zero()
+    counter_1.add([0, 1])
+    assert counter_1.value_display_str() == "1 Packet, 1 Byte"
+    assert not counter_1.is_zero()
+    counter_1.add([2, 66])
+    assert counter_1.value_display_str() == "3 Packets, 67 Bytes"
+    assert not counter_1.is_zero()
+    counter_2 = stats.MultiCounter(group, "Caught Fish", ["Fish", "Gram"], ["Fish", "Grams"])
+    assert counter_2.value_display_str() == "0 Fish, 0 Grams"
+    assert counter_2.is_zero()
+    counter_2.add([1, 0])
+    assert counter_2.value_display_str() == "1 Fish, 0 Grams"
+    assert not counter_2.is_zero()
+    counter_2.add([0, 1])
+    assert counter_2.value_display_str() == "1 Fish, 1 Gram"
+    assert not counter_2.is_zero()
+    counter_2.add([2, 66])
+    assert counter_2.value_display_str() == "3 Fish, 67 Grams"
+    assert not counter_2.is_zero()
