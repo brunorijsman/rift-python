9d290ce268510395c2d81a151a18aad908fdd9a5
Bruno Rijsman <brunorijsman@gmail.com>
2019-02-20 14:40:13 -0300

    Progress on installer

--- install/install.sh
+++ install/install.sh
@@ -1,33 +1,62 @@
 #!/bin/bash
 
-fatal_error () {
-    local message=$1
+report () {
+    message=$1
     echo >&2 "$message"
+}
+
+fatal () {
     exit 1
 }
 
 check_command_present() {
     local command_name=$1
-    command -v $command_name >/dev/null 2>&1 || \
-        fatal_error "Command $command_name is needed but not present on this system"
+    if ! command -v $command_name >/dev/null 2>&1; then
+        report "Command $command_name is needed but not present on this system"
+        fatal
+    fi
 }
 
 check_supported_os() {
+    # Only allow an operating system version that was actually tested
     check_command_present "uname"
     uname_out=$(uname)
     if [ "$uname_out" != "Linux" ]; then
-        fatal_error "Not running on Linux; only Linux is supported (uname reports $uname_out)"
+         report "It appears that you are not running on Linux: uname reports $uname_out"
+         report "The installation script currently only supports Linux"
+         fatal
     fi
     check_command_present "lsb_release"
     distributor_id=$(lsb_release -is)
     if [ "$distributor_id" != "Ubuntu" ]; then
-        fatal_error "Not running on Ubuntu; only Ubuntu is supported (lsb_release reports $distributor_id)"
+         report "It appears that you are not running a different distribution of Linux than Ubuntu"
+         report "lsb_release reports $distributor_id"
+         report "The installation script currently only supports Ubuntu"
+         fatal
     fi
     release=$(lsb_release -rs)
     if [ "$release" != "18.04" ]; then
-        fatal_error "Not running on Ubuntu 18.04; only Ubuntu 18.04 is supported (lsb_release reports $release)"
+         report "It appears that you are running a different version of Ubuntu than 18.04"
+         report "lsb_release reports $release"
+         report "The installation script currently only supports Ubuntu 18.04"
+         fatal
     fi
-    fatal_error "OK"
+}
+
+check_git_directoru () {
+    if [ ! -d ".git" ]; then
+         report "It appears that the current directory is not the root directory of the git repository"
+         report "I don't see a .git subdirectory"
+         report "You must run the installation script from the root directory of the git repository"
+         fatal
+    fi
+    if [ ! -d "rift" ]; then
+         report "It appears that this git repository is not a clone of the rift-python repository"
+         report "I don't see a rift subdirectory"
+         report "You must run the installation script from the root directory of the cloned rift-python git repository"
+         fatal
+    fi
 }
 
 check_supported_os
+check_git_directory
