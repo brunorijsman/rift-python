958c6292e89d999edfcc4bf1354d44c40fb87799
Bruno Rijsman <brunorijsman@gmail.com>
2019-07-17 12:51:04 +0200

    Add support for (non-HMAC) SHA-256 keys

--- rift/key.py
+++ rift/key.py
@@ -1,12 +1,18 @@
+import hashlib
 import hmac
 
 ALGORITHM_TO_DIGESTMOD = {
     "null": "null",                 # Returns an empty digest; used for special key-id 0
-    "hmac-sha-1": "sha1",
+    "hmac-sha-1": "sha1",           # HMAC-SHA-xxx(secret, payload) digest
     "hmac-sha-224": "sha224",
     "hmac-sha-256": "sha256",
     "hmac-sha-384": "sha384",
     "hmac-sha-512": "sha512",
+    "sha-1": "sha1",                # SHA-xxx(secret + playload) digest
+    "sha-224": "sha224",
+    "sha-256": "sha256",
+    "sha-384": "sha384",
+    "sha-512": "sha512",
 }
 
 ALGORITHMS = list(ALGORITHM_TO_DIGESTMOD.keys())
@@ -23,7 +29,7 @@ class Key:
         if self.key_id == 0:
             assert self.algorithm == "null"
             return b''
-        else:
+        elif "hmac" in self.algorithm:
             assert self.algorithm in ALGORITHMS
             digestmod = ALGORITHM_TO_DIGESTMOD[self.algorithm]
             the_hmac = hmac.new(self.secret.encode(), digestmod=digestmod)
@@ -31,6 +37,15 @@ class Key:
                 if message_part is not None:
                     the_hmac.update(message_part)
             return the_hmac.digest()
+        else:
+            assert self.algorithm in ALGORITHMS
+            digestmod = ALGORITHM_TO_DIGESTMOD[self.algorithm]
+            the_hash = hashlib.new(name=digestmod)
+            the_hash.update(self.secret.encode())
+            for message_part in message_parts:
+                if message_part is not None:
+                    the_hash.update(message_part)
+            return the_hash.digest()
 
     def padded_digest(self, message_parts):
         dig = self.digest(message_parts)
