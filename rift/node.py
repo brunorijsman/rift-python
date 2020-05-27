46ede087007111e7c979c0e7542d9b266f5bdc52
Bruno Rijsman <brunorijsman@gmail.com>
2020-05-27 12:29:27 +0200

    add debug code

--- rift/node.py
+++ rift/node.py
@@ -2636,6 +2636,10 @@ class Node:
     def age_ties(self):
         expired_key_ids = []
         for tie_id, tie_packet_info in self.tie_packet_infos.items():
+            ###@@@ DEBUG
+            if tie_packet_info.remaining_tie_lifetime is None:
+                print("tie_packet_info = ", tie_packet_info)
+            ###@@@ 
             tie_packet_info.remaining_tie_lifetime -= 1
             if tie_packet_info.remaining_tie_lifetime <= 0:
                 expired_key_ids.append(tie_id)
