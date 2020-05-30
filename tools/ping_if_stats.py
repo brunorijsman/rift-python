f7f51cd7a52f3760b32697edf5cc9e7538cec5fb
Bruno Rijsman <brunorijsman@gmail.com>
2020-05-30 10:08:41 +0200

    work on ping_if_stats

---
+++ tools/ping_if_stats.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+import sys
+
+ARGS = None
+
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser(description='Ping interface statistics')
+    parser.add_argument('source-ns', help='Ping source namespace name')
+    parser.add_argument('dest-ns', help='Ping destination namespace name')
+    parser.add_argument('stats-ns', help='Interface statistics namespace name')
+    args = parser.parse_args()
+    return args
+
+def fatal_error(error_msg):
+    print(error_msg, file=sys.stderr)
+    sys.exit(1)
+
+def ping_interface_stats(source_ns, dest_ns, stats_ns):
+    if not namespace_exists(source_ns):
+        fatal_error("Source namespace {} does not exist".format(source_ns))
+    if not namespace_exists(dest_ns):
+        fatal_error("Destination namespace {} does not exist".format(dest_ns))
+    if not namespace_exists(stats_ns):
+        fatal_error("Statistics namespace {} does not exist".format(stats_ns))
+    ###@@@
+
+def namespace_exists(ns_name):
+    try:
+        result = subprocess.run(['ip', 'netns', 'pids', ns_name], stdout=subprocess.PIPE)
+    except FileNotFoundError:
+        fatal_error('"ip" command not found')
+    print(result)
+    output = result.stdout.decode('ascii')
+    print(output)
+    return True
+
+def main():
+    # pylint:disable=global-statement
+    global ARGS
+    ARGS = parse_command_line_arguments()
+    source_ns = getattr(ARGS, 'source-ns')
+    dest_ns = getattr(ARGS, 'dest-ns')
+    stats_ns = getattr(ARGS, 'stats-ns')
+    ping_interface_stats(source_ns, dest_ns, stats_ns)
+
+if __name__ == "__main__":
+    main()
