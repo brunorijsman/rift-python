a99bfe1e1706352d4583df3f4bd3a88aa9c9c06a
Bruno Rijsman <brunorijsman@gmail.com>
2020-05-31 21:33:53 +0200

    fix conversions

--- rift/kernel.py
+++ rift/kernel.py
@@ -118,8 +118,8 @@ class Kernel:
         routes = self.ipr.get_routes()
         for route in routes:
             if route["proto"] == RTPROT_RIFT:
-                dst_prefix_str = self.kernel_route_dst_prefix_str(route)
-                self.del_route(dst_prefix_str)
+                prefix = packet_common.make_ip_prefix(self.kernel_route_dst_prefix_str(route))
+                self.del_route(prefix)
 
     def nhop_to_kernel_args(self, nhop, dst):
         link = self.ipr.link_lookup(ifname=nhop.interface)
