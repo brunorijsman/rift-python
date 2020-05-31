468ae2fcf4205373e8d6e427cb364f82e5011fb7
Bruno Rijsman <brunorijsman@gmail.com>
2020-05-31 10:10:16 +0200

    disagg check

--- tools/config_generator.py
+++ tools/config_generator.py
@@ -954,7 +954,8 @@ class Node:
         step = "There is no disaggregation"
         parsed_disagg = self.telnet_session.parse_show_output("show disaggregation")
         same_level_nodes = parsed_disagg[0]
-        print(same_level_nodes)
+        for row in same_level_nodes[1:]:
+            print(row)
 
 
         _partial_interfaces = parsed_disagg[1]
