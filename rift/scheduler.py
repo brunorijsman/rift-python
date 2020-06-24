eaacb926ef11c7bbbb6eef26c31493f822e43b5b
Bruno Rijsman <brunorijsman@gmail.com>
2020-06-24 16:01:54 +0200

    debug long queue time

--- rift/scheduler.py
+++ rift/scheduler.py
@@ -29,17 +29,13 @@ class Scheduler:
             while Fsm.events_pending() or TIMER_SCHEDULER.expired_timers_pending():
                 Fsm.process_queued_events()
                 timeout = TIMER_SCHEDULER.trigger_all_expired_timers()
-                ###@@@
-                if timeout is not None and timeout > 2.0:
-                    print("Long timeout:", timeout)
-                ###@@@
             if timeout:
-                call_select_time = time.monotonic()
+                start_time = time.monotonic()
             rx_ready, _, _ = select.select(self._rx_fds, [], [], timeout)
             if timeout:
-                actual_select_time = time.monotonic() - call_select_time
+                actual_select_time = time.monotonic() - start_time
                 if actual_select_time > timeout:
-                    slip_time = actual_select_time - timeout
+                    slip_time = start_time - timeout
                     if slip_time > 0.01:
                         self.slip_count_10ms += 1
                     if slip_time > 0.1:
@@ -47,7 +43,16 @@ class Scheduler:
                     if slip_time > 1.0:
                         self.slip_count_1000ms += 1
             for rx_fd in rx_ready:
+                ###@@@vvv
+                start_time = time.monotonic()
+                ###@@@^^^
                 handler = self._handlers_by_rx_fd[rx_fd]
                 handler.ready_to_read()
+                ###@@@vvv
+                read_time = time.monotonic() - start_time
+                if read_time > 1.0:
+                    print("LONG READ TIME:", read_time)
+                ###@@@^^^
+
 
 SCHEDULER = Scheduler()
