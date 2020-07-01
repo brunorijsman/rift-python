96924ee06894b36d639d8ed678aa792c285517e7
Bruno Rijsman <brunorijsman@gmail.com>
2020-07-01 11:15:37 +0200

    Replace TODO with issue #74

--- tools/config_generator.py
+++ tools/config_generator.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-# TODO: Add check to verify that there is not an excessive rate of packets on each interface
-#       after convergence (this requires that rate reporting if made sane first)
-
 # TODO: Add a check to look for WARNING/ERROR/CRITICAL messages in log
 
 # TODO: Have a separate log file per process when running in multi-process simulation
