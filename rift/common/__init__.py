897ffbfbd828fe17a2d3e3d3049f4c002a6cd7da
prz@zeta2.ch <prz@zeta2.ch>
2023-10-12 19:12:33 +0200

    newest schemas

--- rift/common/__init__.py
+++ rift/common/__init__.py
@@ -1,11 +1,9 @@
-#very, very magic code to cleanup the immutable schema to allow old code to modify rift packets
-
+#
+#
+# $Id
+#
+# Copyright (c) 2021, Juniper Networks, Inc.
+# All rights reserved.
+#
+#
 __all__ = ['ttypes', 'constants']
-
-from . import ttypes
-
-for _klass in dir(ttypes):
-    try:
-        delattr(getattr(ttypes, _klass),"__setattr__")
-    except (KeyError, AttributeError):
-        pass
