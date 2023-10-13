63fe5d0d5febd74026364696055a9ef7a9402838
prz@zeta2.ch <prz@zeta2.ch>
2023-10-13 19:14:24 +0200

    newest 3.10 support

--- rift/common/__init__.py
+++ rift/common/__init__.py
@@ -7,3 +7,12 @@
 #
 #
 __all__ = ['ttypes', 'constants']
+
+from . import ttypes
+
+for _klass in dir(ttypes):
+    try:
+        delattr(getattr(ttypes, _klass),"__setattr__")
+    except (KeyError, AttributeError):
+        pass
+
