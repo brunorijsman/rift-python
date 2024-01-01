5ebb1ba1d47a9fb724d76106f9ae4947d289a610
prz@zeta2.ch <prz@zeta2.ch>
2024-01-01 15:55:34 +0100

    usual tomfoolery with the ever incompatible python versions

--- rift/encoding/__init__.py
+++ rift/encoding/__init__.py
@@ -1,2 +1,12 @@
+#very, very magic code to cleanup the immutable schema to allow old code to modify rift packets
 
 __all__ = ['ttypes', 'constants']
+
+from . import ttypes
+
+for _klass in dir(ttypes):
+    try:
+        delattr(getattr(ttypes, _klass),"__setattr__")
+    except (KeyError, AttributeError):
+        pass
+
